038c6a62567dbf52ff347236821c6544ded1e181
xuchen <kingxc1991@foxmail.com>
2024-01-29 14:17:19 +0800

    feat: 添加 phind 的支持

--- src/background.ts
+++ src/background.ts
@@ -1,33 +1,45 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'changeModel' && request.payload.model) {
-    chrome.cookies.get({
-      name: 'chat_mode',
-      url: 'https://you.com'
-    }).then(async oldModel => {
-      if (oldModel && oldModel.value !== request.payload.model) {
-        await chrome.cookies.remove({
-          name: 'chat_mode',
-          url: 'https://you.com'
-        })
-        await chrome.cookies.set({
-          url: 'https://you.com',
-          name: 'chat_mode',
-          value: request.payload.model,
-          domain: 'you.com',
-          expirationDate: 9999999999,
-          secure: true
-        })
-        sendResponse({
-          action: 'changeModel',
-          payload: true,
-        })
-      } else {
-        sendResponse({
-          action: 'changeModel',
-          payload: false,
-        })
-      }
-    })
+  if (request.action === 'changeModel' && request.payload.value && request.payload.name) {
+    if (request.payload.storageLocation === 'cookie') {
+      chrome.cookies.get({
+        name: request.payload.key,
+        url: request.payload.url
+      }).then(async oldModel => {
+        if (oldModel && oldModel.value !== request.payload.model) {
+          await chrome.cookies.remove({
+            name: 'chat_mode',
+            url: 'https://you.com'
+          })
+          await chrome.cookies.set({
+            url: request.payload.url,
+            name: request.payload.key,
+            value: request.payload.value,
+            domain: request.payload.domain,
+            expirationDate: 9999999999,
+            secure: true
+          })
+          sendResponse({
+            action: 'changeModel',
+            payload: true,
+          })
+        } else {
+          sendResponse({
+            action: 'changeModel',
+            payload: false,
+          })
+        }
+      })
+    }
+    if (request.payload.storageLocation === 'localstorage') {
+      console.log('修改 localstorage', request.payload)
+      chrome.storage.local.set({
+        [request.payload.key]: request.payload.value
+      })
+      sendResponse({
+        action: 'changeModel',
+        payload: true,
+      })
+    }
   }
   return true;
 });
--- src/content.tsx
+++ src/content.tsx
@@ -1,16 +1,22 @@
 // 只做一件事, 就是将 url 中的参数解析出来, 然后传递给 App 组件
 
 const pathname = window.location.pathname;
+const host = window.location.host;
 
 // 支持的白名单模型
-const whiteModel: Record<string, boolean> = {
+const youWhiteModel: Record<string, boolean> = {
   'gpt-4': true,
   default: true,
   'agent': true,
   'research': true,
   'create': true,
 }
+const hindWhiteModel: Record<string, boolean> = {
+  'gpt-4': true,
+  'Phind Model': true,
+}
 if (
+  host === 'you.com' &&
   pathname === '/search' &&
   window.location.search?.length > 0) {
   const searchParams = new URLSearchParams(window.location.search);
@@ -18,11 +24,15 @@ if (
   const cid = searchParams.get('cid');
   if (tbm && tbm === 'youchat' && !cid) {
     const model = searchParams.get('chat_mode') as string;
-    if (model && whiteModel[model]) {
+    if (model && youWhiteModel[model]) {
       chrome.runtime.sendMessage({
         action: 'changeModel',
         payload: {
-          model
+          storageLocation: 'cookie', // 存储位置
+          key: 'chat_mode',
+          value: model,
+          url: 'https://you.com',
+          domain: 'you.com',
         }
       }, response => {
         if (response.action === 'changeModel') {
@@ -31,6 +41,27 @@ if (
           }
         }
       })
+    }
+  }
+}
+console.log('host', host);
+if (
+  host === 'www.phind.com' &&
+  pathname === '/search' &&
+  window.location.search?.length > 0
+) {
+  const searchParams = new URLSearchParams(window.location.search);
+  const cache = searchParams.get('cache');
+  if (!cache) {
+    const model = searchParams.get('answerModel') as string;
+    console.log('phind Model', model)
+    if (model && hindWhiteModel[model]) {
+      const oldModel = window.localStorage.getItem('answerModel');
+      if (oldModel && oldModel !== `"${model}"`) {
+        window.stop();
+        window.localStorage.setItem('answerModel', `"${model}"`);
+        window.location.reload();
+      }
     }
   }
 }
